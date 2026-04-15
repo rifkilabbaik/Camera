67ec1b2c26857d26c2b37d217dde8aae575f56d1
rifkilabbaik <rifkilabbaik@gmail.com>
2026-04-15 19:43:54 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,19 @@
-const CACHE_NAME = "kamera-app-v1";
+const CACHE_NAME = "kamera-lokasi-v1";
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll([
-      "/",
-      "/index.html"
-    ]))
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
